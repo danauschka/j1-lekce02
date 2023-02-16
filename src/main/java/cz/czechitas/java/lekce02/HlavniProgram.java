b0e2d04d463e2acb2be35303dbda43a870130cf4
Dana Goubejova <dana.goubejova@gmail.com>
2023-02-16 19:45:42 +0100

    Postupne se nakresli domecky a pak prasatko.

--- src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/lekce02/HlavniProgram.java
@@ -7,8 +7,112 @@ public class HlavniProgram {
     public void start() {
         zofka = new Turtle();
 
-        //TODO tady bude kód
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(400);
+        zofka.turnRight(90);
+        zofka.penDown();
+
+        nakresliDomecky();
+
+        zofka.turnLeft(180);
+        zofka.penUp();
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(400);
+        zofka.penDown();
+
+        nakresliObdelnik();
+        zofka.move(120);
+
+        nakresliTrojuhelnik();
+        zofka.turnRight(15);
+
+        nakresliNohy();
+
+        zofka.turnRight(60);
+        zofka.penUp();
+        zofka.move(120);
+        zofka.penDown();
+        zofka.turnRight(60);
+
+        nakresliNohy();
+
+        /*
+        zofka.move(120);
+        nakresliTrojuhelnik();
+
+        zofka.turnRight(15);
+
+        nakresliNohy();
+
+        zofka.turnRight(60);
+        zofka.penUp();
+        zofka.move(120);
+        zofka.penDown();
+        zofka.turnRight(60);
+
+        nakresliNohy();
+        */
+
+
     }
+
+
+    private void nakresliObdelnik() {
+        for (int i = 0; i < 2; i++) {
+            zofka.move(120);
+            zofka.turnLeft(90);
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+    }
+    private void nakresliTrojuhelnik() {
+
+        zofka.turnLeft(45);
+        zofka.move(70);
+        zofka.turnLeft(90);
+        zofka.move(70);
+    }
+
+    private void nakresliDomecek() {
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+    }
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.move(70);
+        zofka.turnLeft(90);
+        zofka.move(70);
+    }
+
+    private void nakresliDomecky() {
+        for (int i = 0; i < 4; i++) {
+            nakresliDomecek();
+            zofka.turnLeft(45);
+            zofka.penUp();
+            zofka.move(100);
+            zofka.turnLeft(90);
+            zofka.move(300);
+            zofka.turnLeft(90);
+            zofka.penDown();
+        }
+    }
+
+        private void nakresliNohy() {
+
+        zofka.move(35);
+        zofka.turnRight(120);
+        zofka.penUp();
+        zofka.move(35);
+        zofka.turnRight(120);
+        zofka.penDown();
+        zofka.move(35);
+    }
+
+
+
 
     public static void main(String[] args) {
         new HlavniProgram().start();
